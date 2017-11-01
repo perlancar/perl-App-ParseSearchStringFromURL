a26da64e567d3b410fab47606838cc641273ff64
perlancar (on pc-office) <perlancar@gmail.com>
2017-11-01 16:33:10 +0700

    Initial commit

---
+++ lib/App/ParseSearchStringFromURL.pm
@@ -0,0 +1,73 @@
+package App::ParseSearchStringFromURL;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+$SPEC{parse_search_string_from_url} = {
+    v => 1.1,
+    summary => 'Parse search string from URL',
+    args => {
+        urls => {
+            'x.name.is_plural' => 1,
+            'x.name.singular' => 'url',
+            req => 1,
+            pos => 0,
+            greedy => 1,
+            cmdline_src => 'stdin_or_args',
+            stream => 1,
+        },
+        detail => {
+            summary => 'If set to true, will also output other '.
+                'components aside from search string',
+            cmdline_aliases => {l=>{}},
+        },
+        module => {
+            schema => ['str*', in=>[
+                'URI::ParseSearchString',
+                'URI::ParseSearchString::More',
+            ]],
+            default => 'URI::ParseSearchString',
+        },
+    },
+    result => {
+        stream => 1,
+    },
+};
+sub parse_search_string_from_url {
+    my %args = @_;
+
+    my $urls = $args{urls};
+    my $detail = $args{detail};
+    my $mod = $args{module};
+
+    (my $mod_pm = "$mod.pm") =~ s!::!/!g;
+    require $mod_pm;
+
+    if ($mod =~ /^URI::ParseSearchString(?:::More)?$/) {
+        my $uparse = $mod->new;
+        return sub {
+            my $url = $urls->();
+            return undef unless defined $url;
+            if ($detail) {
+                return {
+                    host          => $uparse->se_host($url),
+                    name          => $uparse->se_name($url),
+                    search_string => $uparse->se_term($url),
+                };
+            } else {
+                return $uparse->se_term($url);
+            }
+        };
+    }
+}
+
+1;
+#ABSTRACT:
+
+=cut
